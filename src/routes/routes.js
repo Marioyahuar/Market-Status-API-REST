3297ef9aaa03b3a36e674a0767888591488546f8
Marioyahuar <marioyahuar@gmail.com>
2023-02-10 15:44:41 -0500

    Folder structure and routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-router.get('/',(req,res) => {
-    res.end('Welcome');
-})
+router.get("/orderbook/:pair", (req, res) => {
+    const {pair} = req.params;
+    return res.status(400).send(pair);
+});
+
+router.get("/market-depth/:pair/:type/:amount", (req, res) => {
+    const {pair, type, amount} = req.params;
+    return res.status(400).send(amount);
+});
 
 module.exports = router;
-
